86104fd43c14fef84c5eb94db640042df1a5959e
shanunited <ss4696@njit.edu>
2023-08-04 13:52:22 -0400

    EDA DATA

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from src.exception import CustomExecption
+from src.logger import logger
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the Data ingestion method or component")
+        try:
+            pass
+        except:
+            pass
+        
